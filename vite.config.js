3d9dc15df2dac9032732275d7300c4bba4a44ce7
mirrorgo <cloudmirrorgo@163.com>
2022-01-20 21:09:10 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const { resolve } = require("path");
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        //用于配置多页面应用
+        main: resolve(__dirname, "index.html"), //http://localhost:3000 和 http://localhost:3000/index.html 都可以访问
+        nested: resolve(__dirname, "/taojin/options.html"), //http://localhost:3000/taojin/options.html
+      },
+    },
+  },
+});
