d988d1aa4b665c9ce695605b59de44c94f1b0a74
mirrorgo <cloudmirrorgo@163.com>
2022-01-24 17:20:19 +0800

    引入slate,准备基于slate进行二次开发(目标:支持markdown基础语法)

--- taojin/collections/components/RichText/index.jsx
+++ taojin/collections/components/RichText/index.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+import "./index.css";
 
 export default function RichText() {
+  // 创建一个不会在渲染中变化的 Slate 编辑器对象。
+  const editor = useMemo(() => withReact(createEditor()), []);
+  //跟踪编辑器中value的值
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ]);
   return (
     <div>
       <h3>RichText</h3>
-      {/* mdn上官方的例子已经废弃了,得重新找一个 */}
-      <div id="textBox" contenteditable="true">
-        <p>placeholder</p>
+      {/* <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)} /> */}
+      <div className="editor">
+        <Slate
+          editor={editor}
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        >
+          <Editable
+            // 定义一个新的处理程序在控制台打印按下的键
+            onKeyDown={(event) => {
+              console.log(event.key);
+            }}
+          />
+        </Slate>
       </div>
     </div>
   );
