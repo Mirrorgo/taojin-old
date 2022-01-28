ba29a6e947c3f633a4151535579e98027b6ade60
mirrorgo <cloudmirrorgo@163.com>
2022-01-28 22:00:35 +0800

    准备svg

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 const { resolve } = require("path");
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import viteSvgIcons from "vite-plugin-svg-icons"; //svg
-// import path from "path"; //FIXME 是否与第一个相同? svg
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    //引入svg
-    // viteSvgIcons({
-    //   // Specify the icon folder to be cached
-    //   iconDirs: [path.resolve(process.cwd(), "src/icons")],
-    //   // Specify symbolId format
-    //   symbolId: "icon-[dir]-[name]",
-    // }),
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       input: {
