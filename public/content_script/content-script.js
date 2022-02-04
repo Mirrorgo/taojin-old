cc73922059487c4b5c363491dadf387f0ed19498
Cavetr <17785156792@163.com>
2022-02-04 22:49:46 +0800

    还有一堆问题

--- public/content_script/content-script.js
+++ public/content_script/content-script.js
@@ -1,9 +1,9 @@
 //// content-script.js ////
+document.body.style.backgroundColor = "orange";
 let searchbar = document.createElement("iframe");
-searchbar.src = chrome.runtime.getURL("index.html");
-// "chrome-extension://llgmdnnodeomlbdnkckaineblidoahia/taojin/searchbar.html";
-// "https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik";
-searchbar.id = "taojin-searchbar";
-// searchbar.sandbox = "allow-same-origin allow-scripts";
-// document.body.insertBefore(searchbar, document.body.firstChild);
-document.body.appendChild(searchbar);
+searchbar.src = chrome.runtime.getURL("../../taojin/searchbar/index.html");
+searchbar.className = "taojin-searchbar";
+searchbar.sandbox = "allow-same-origin allow-scripts allow-modals allow-popups";
+document.body.append(searchbar);
+
+
