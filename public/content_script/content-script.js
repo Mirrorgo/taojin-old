7d843d0fd8a4ff82b3d8a5d45685e19346a36128
Cavetr <17785156792@163.com>
2022-02-08 18:08:11 +0800

    样式改完了，打算再加上搜索提示和收起来的功能

--- public/content_script/content-script.js
+++ public/content_script/content-script.js
@@ -1,18 +1,27 @@
 //// content-script.js ////
-
-
-document.body.style.backgroundColor = "orange";
 // 创建搜索框
 document.addEventListener('keydown', addSearchBar());
 const searchBar = document.createElement("div");
+const searchTools = [
+  {
+    name: "bing",
+    url: "https://cn.bing.com/search?q=",
+  }, {
+    name: "baidu",
+    url: "https://www.baidu.com/s?wd=",
+  }
+];
 let show = true;
 searchBar.id = "taojin-searchbar";
 searchBar.className = "taojin-searchbar";
 
 const inputEl = document.createElement("input");
-inputEl.placeholder = "搜索网页";
 inputEl.id = "search-input";
-inputEl.addEventListener('keydown', search);
+inputEl.className = "search-input";
+inputEl.placeholder = searchTools[0].name;
+inputEl.addEventListener('keydown', search());
+// inputEl.addEventListener('blur',controlSearchBar);
+
 
 searchBar.appendChild(inputEl);
 // 是否显示搜索框
@@ -20,6 +29,7 @@ function controlSearchBar() {
   console.log(show, searchBar);
   if (show) {
     document.body.appendChild(searchBar);
+    inputEl.focus();
   } else {
     searchBar.remove();
   }
@@ -39,29 +49,49 @@ function addSearchBar() {
     }
   }
 }
-// 搜索
-function search(e) {
-  console.log(e);
-  const searchValue = document.getElementById("search-input").value;
-  if (e.key === "Enter" && searchValue) {
-    controlSearchBar();
-    addSearchContent(`https://cn.bing.com/search?q=${searchValue}`);
-    // addSearchContent("https://www.baidu.com/s?wd=ssd");
+// 输入框的键盘事件
+function search() {
+  let searchTool = 0;
+  return function (e) {
+    console.log(e);
+    const inputEl = document.getElementById("search-input");
+    if (e.key === "Tab") {
+      console.log("tab");
+      searchTool = (searchTool + 1) % searchTools.length;
+      inputEl.placeholder = searchTools[searchTool].name;
+      inputEl.focus();
+      e.preventDefault();
+    }
+    const searchValue = inputEl.value;
+    if (e.key === "Enter" && searchValue) {
+      console.log(inputEl)
+      inputEl.value = "";
+      controlSearchBar();
+      addSearchContent(searchTools[searchTool].url + searchValue);
+      return false;
 
+    }
   }
+
 }
 // 添加搜索内容
+let maxZindex = 9999;
 function addSearchContent(url) {
   // 搜索结果框体
-  const searchContentBox = document.createElement("span");
+  const searchContentBox = document.createElement("div");
   searchContentBox.className = "search-content-box";
-
+  searchContentBox.style.zIndex = maxZindex++;
+  searchContentBox.addEventListener('mousedown', function () {
+    searchContentBox.style.zIndex = maxZindex++;
+    console.log(maxZindex);
+  })
   // 顶部
   const searchContentBar = document.createElement("div");
   searchContentBar.className = "search-content-bar";
   move(searchContentBox, searchContentBar);
   // 关闭
   const deleteButton = document.createElement("button");
+  deleteButton.className = "delete-button";
   deleteButton.innerHTML = "x";
   deleteButton.addEventListener("click", function () {
     searchContentBox.remove()
@@ -79,56 +109,23 @@ function addSearchContent(url) {
   searchContentBox.appendChild(searchContent);
   document.body.appendChild(searchContentBox);
 }
-
+// 拖动
 function move(box, moveBar) {
-  //元素鼠标按下事件   
-  //当鼠标在这个元素身上按下的时候 就会触发回调函数  
-  moveBar.onmousedown = function (event) {
-    // 获取元素起始位置
-    let eleX = box.offsetLeft;
-    let eleY = box.offsetTop;
-    // 鼠标按下的坐标
-    let startX = event.clientX;
-    let startY = event.clientY;
-    // 解决方案：开启全局捕获
-    // 把所有后续的鼠标事件全部强制到这个元素身上，不让事件去处理文字
-    moveBar.setCapture && moveBar.setCapture();
-    //鼠标抬起事件
-    document.onmousemove = throttle(function (event) {
-      console.log('移动');
-      // 获取鼠标移动后的位置
-      let endX = event.clientX;
-      let endY = event.clientY;
-      // 鼠标距离差 = 鼠标移动后位置 - 鼠标按下的位置
-      let disX = endX - startX;
-      let disY = endY - startY;
-      // 元素最终的位置 = 元素初始位置 + 鼠标距离差
-      let lastX = eleX + disX;
-      let lastY = eleY + disY;
-
-      // 边界值判断  (吸附效果)
-      // 边界值判断逻辑 大于最大值等于最大值 小于最小值等于最小值
-      if (lastX > document.documentElement.clientWidth - box.offsetWidth - 20) {
-        lastX = document.documentElement.clientWidth - box.offsetWidth;
-      } else if (lastX < 20) {
-        lastX = 0;
-      }
-      if (lastY > document.documentElement.clientHeight - box.offsetHeight - 20) {
-        lastY = document.documentElement.clientHeight - box.offsetHeight;
-      } else if (lastY < 20) {
-        lastY = 0;
-      }
-      // 设置元素移动位置
-      box.style.left = lastX + 'px';
-      box.style.top = lastY + 'px';
-    },30);
-    //鼠标抬起事件
+  moveBar.addEventListener("mousedown", function (e) {
+    const eleX = box.offsetLeft;
+    const startX = e.clientX;
     document.onmouseup = function () {
       document.onmousemove = null;
-      moveBar.releaseCapture && moveBar.releaseCapture();
     };
+    document.onmousemove = throttle(function (e) {
+      let lastX = eleX + e.clientX - startX;
+      if (lastX < 0) {
+        lastX = 0;
+      }
+      box.style.left = lastX + 'px';
+    }, 10);
     return false;
-  };
+  });
 }
 // 节流
 function throttle(func, wait) {
